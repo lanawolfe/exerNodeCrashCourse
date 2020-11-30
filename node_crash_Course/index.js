b8bb4d423b7b74d47a47bd787744be28a9ddf3d9
lanawolfe <lana.j.wolfe@utah.edu>
2020-11-30 15:14:50 -0700

    final

--- node_crash_Course/index.js
+++ node_crash_Course/index.js
@@ -1,25 +1,103 @@
-//const Logger = require('./logger');
-//const logger = new Logger();
+// //const Logger = require('./logger');
+// //const logger = new Logger();
 
-//logger.on('message', data => console.log('Called listener', data));
+// //logger.on('message', data => console.log('Called listener', data));
 
-//logger.log('hello world');
-//logger.log('hi');
-//logger.log('hello');
+// //logger.log('hello world');
+// //logger.log('hi');
+// //logger.log('hello');
 
 
-//console.log('Hello From Node.JS...');
+// //console.log('Hello From Node.JS...');
 
-const http = require('http');
-const path = require('path');
-const fs = require('fs');
+ const http = require('http');
+ const path = require('path');
+ const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-if(req.url === '/'){
-    res.end('<h1>HOMEPAGE</h1>');
+ const server = http.createServer((req, res) => {
+// if(req.url === '/'){
+//     fs.readFile(
+//         path.join(__dirname, 'public', 'index.html'), 
+//         (err, content) => {
+//         if (err) throw err;
+//     res.writeHead(200, { 'content-Type': 'text/html' });
+//     res.end(content);
+        
+//         }
+//     );
+// } 
+                         
+//     if(req.url === '/api/users'){
+//         const users = [
+//             {name: 'Bob Smith', age: 40},
+//             {name: 'John Doe', age: 30}
+//         ];
+//         res.writeHead(200, { 'content-Type': 'application/json' });
+//         res.end(JSON.stringify(users));
+//     }
+//Build file path
+let filePath = path.join(
+    __dirname,
+     'public', 
+     req.url === '/' ? 'index.html' : req.url
+     );
+
+//extension of file
+let extname = path.extname(filePath);
+//initial content type
+let contentType = 'text/html';
+//check ext and set content type
+switch(extname) {
+    case '.js':
+    contentType = 'text/javascript';
+    break;
+
+    case '.css':
+    contentType = 'text/css';
+    break;
+
+    case '.json':
+    contentType = 'application/json';
+    break;
+
+    case '.png':
+    contentType = 'image/png';
+    break;
+
+    case '.jpg':
+    contentType = 'image/jpg';
+    break;
 }
+
+//read file
+fs.readFile(filePath, (err, content) => {
+    if(err) { 
+        if(err.code == 'ENOENT') {
+            //page not found
+            fs.readFile(path.join(__dirname, 'public', '404.html'),
+            (err, content) => { 
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(content, 'utf8');
+            })
+        } else {
+            //some server error
+            res.writeHead(500);
+            res.end(`server Error: ${err.code}`);
+        }
+    } else {
+        //success
+        res.writeHead(200, { 'Content-Type': contentType });
+        res.end(content, 'utf8');
+    }
 });
+
+//console.log(filePath);
+//res.end();
+
+});   
+                            
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () =>console.log(`server running on port ${PORT}`));
+ server.listen(PORT, () => console.log(`server running on port ${PORT}`));
+
